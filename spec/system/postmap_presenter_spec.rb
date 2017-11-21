c0165a05547c3fe3d679a46cba31672926306dd1
Steffen Uhlig <steffen@familie-uhlig.net>
2017-11-21 22:08:06 +0100

    Fix system test for PostmapPresenter

--- spec/system/postmap_presenter_spec.rb
+++ spec/system/postmap_presenter_spec.rb
@@ -2,17 +2,19 @@
 
 require 'spec_helper'
 require 'tempfile'
-require 'sgh/elternverteiler/eltern_presenter'
+require 'sgh/elternverteiler/postmap_presenter'
 
-describe ElternPresenter, type: 'system' do
+describe PostmapPresenter, type: 'system' do
+  let(:k4a) { Klasse.new(stufe: '4', zug: 'a').save }
+
   before do
-    Schüler.new(vorname: 'Bart', nachname: 'Simpson', klasse: '4a').save
+    Schüler.new(vorname: 'Bart', nachname: 'Simpson', klasse: k4a).save
     Erziehungsberechtigter.new(vorname: 'Homer', nachname: 'Simpson', mail: 'homer@simpson.org').save
     Erziehungsberechtigter.new(vorname: 'Marge', nachname: 'Simpson', mail: 'marge@simpson.org').save
-    Schüler.new(vorname: 'Milhouse', nachname: 'Van Houten', klasse: '4a').save
+    Schüler.new(vorname: 'Milhouse', nachname: 'Van Houten', klasse: k4a).save
     Erziehungsberechtigter.new(vorname: 'Luann', nachname: 'Van Houten', mail: 'luann@vanhouten.org').save
     Erziehungsberechtigter.new(vorname: 'Kirk', nachname: 'Van Houten', mail: 'kirk@vanhouten.org').save
-    Schüler.new(vorname: 'Nelson', nachname: 'Muntz', klasse: '4a').save
+    Schüler.new(vorname: 'Nelson', nachname: 'Muntz', klasse: k4a).save
     Erziehungsberechtigter.new(vorname: 'Eddie', nachname: 'Muntz', mail: 'eddie@muntz.org').save
     Erziehungsberechtigter.new(nachname: 'Muntz').save
   end
@@ -26,7 +28,11 @@ describe ElternPresenter, type: 'system' do
   end
 
   before do
-    File.write(@db_file.path, described_class.new('eltern@springfield-elementary.edu').to_s)
+    File.write(@db_file.path, described_class.new('eltern@springfield-elementary.edu').present(Erziehungsberechtigter.all))
+  end
+
+  it 'contains a sizable amount of characters' do
+    expect(@db_file.size).to be > 100
   end
 
   it 'is parsable by postmap without errors' do
