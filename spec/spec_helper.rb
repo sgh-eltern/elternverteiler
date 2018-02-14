21d7333eb6fb7905a0a4f8a4e24dcb480a631af8
Steffen Uhlig <steffen@familie-uhlig.net>
2018-02-14 21:27:47 +0100

    Allow focus via fit, fcontext etc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,9 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.filter_run_when_matching :focus
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   # config.disable_monkey_patching!
   config.warnings = false
 
