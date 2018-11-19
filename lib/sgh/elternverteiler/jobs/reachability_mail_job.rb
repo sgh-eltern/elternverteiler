ed23cc016503ed87889481a04a4ffc8e0cb81164
Steffen Uhlig <steffen@familie-uhlig.net>
2018-11-19 22:31:19 +0100

    Fix TLS options for production mail server

--- lib/sgh/elternverteiler/jobs/reachability_mail_job.rb
+++ lib/sgh/elternverteiler/jobs/reachability_mail_job.rb
@@ -19,18 +19,26 @@ module SGH
           body: body,
           html_body: html_body,
           via: :smtp,
-          via_options: {
-            address: ENV.fetch('smtp_host'),
-            port: ENV.fetch('smtp_port'),
-            # tls: !!ENV.fetch('smtp_tls'),
-            enable_starttls_auto: true,
-            user_name: ENV.fetch('smtp_user'),
-            password: ENV.fetch('smtp_password'),
-            authentication: ENV.fetch('smtp_authentication'),
-            domain: ENV.fetch('smtp_host'),
-          }
+          via_options: via_options
         )
         finish
+      end
+
+      private
+
+      def via_options
+        {
+          address: ENV.fetch('smtp_host'),
+          port: ENV.fetch('smtp_port'),
+          enable_starttls_auto: true,
+          user_name: ENV.fetch('smtp_user'),
+          password: ENV.fetch('smtp_password'),
+          authentication: ENV['smtp_authentication'],
+          domain: ENV.fetch('smtp_host')
+        }.tap do |via_options|
+          smtp_tls = ENV['smtp_tls']
+          via_options[:tls] = !!smtp_tls if smtp_tls
+        end
       end
     end
   end
